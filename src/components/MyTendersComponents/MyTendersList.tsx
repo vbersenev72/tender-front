fb3ac552e23eb400c9e911ea7c9035718f0b61aa
Vladik <vbersenev72@gmail.com>
2024-01-17 15:40:27 +0500

    mytenders

---
+++ src/components/MyTendersComponents/MyTendersList.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IMyTendersListProps {
+}
+
+export function MyTendersList ({jsonData}:any) {
+  return (
+    <div>
+
+    </div>
+  );
+}
--- src/pages/MyTenders/MyTendesPage.tsx
+++ src/pages/MyTenders/MyTendesPage.tsx
@@ -6,6 +6,9 @@ import axios from 'axios';
 import { showErrorMessage, showSuccesMessage } from '../../functions/Message';
 import { TextBlack22pxRegular } from '../../constants/fonts';
 import './MyTendersPage.css'
+import { SiMicrosoftexcel } from "react-icons/si";
+import { RiFileExcel2Line } from "react-icons/ri";
+
 
 export interface IMyTendersPageProps {
 }
@@ -55,24 +58,34 @@ export function MyTendersPage(props: IMyTendersPageProps) {
         :
         <div className='Mytenders-container'>
 
-          <div className="Mytenders-preview">
-            <TextBlack22pxRegular>Мои тендеры</TextBlack22pxRegular>
-            <div className="preview-properties">
-              <div className="preview-property"><p>Все</p></div>
-              <div className="preview-property"><p>Текущие</p></div>
-              <div className="preview-property"><p>Завершенные</p></div>
-              <div className="preview-property"><p>За период</p></div>
+          <div className='Mytenders-content'>
+            <div className="Mytenders-preview">
+              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '30px', width: 'fit-content'}}><p>Мои тендеры</p></div>
+              <div className="preview-properties">
+                <div className="preview-property"><p>Все</p></div>
+                <div className="preview-property"><p>Текущие</p></div>
+                <div className="preview-property"><p>Завершенные</p></div>
+                <div className="preview-property"><p>За период</p></div>
+              </div>
+            </div>
+
+            <div className="Mytenders-sort">
+              <div className='Mytenders-sort-list'>
+                <div style={{ color: 'gray', paddingLeft: '0px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingRight: '15px' }}><p>Сортировать по</p></div>
+                <div className="sort-property"><p>Дата добавления в мои тендеры</p></div>
+                <div className="sort-property"><p>Дата начала подачи заявок</p></div>
+                <div className="sort-property"><p>Цена</p></div>
+                <div className="sort-property"><p>Дата окончания</p></div>
+                <div className="sort-property"><p>Дата публикации</p></div>
+              </div>
+              <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', padding: '15px', float: 'right'}}>
+              <RiFileExcel2Line size={30} color='#3294F4' />
+              </div>
             </div>
           </div>
 
-          <div className="Mytenders-sort">
-            <div className="sort-property"><p>Сортировать по</p></div>
-            <div className="sort-property"><p>Дата добавления в мои тендеры</p></div>
-            <div className="sort-property"><p>Дата добавления начала подачи заявок</p></div>
-            <div className="sort-property"><p>Цена</p></div>
-            <div className="sort-property"><p>Дата окончания</p></div>
-            <div className="sort-property"><p>Дата публикации</p></div>
-          </div>
+
+
 
         </div>}
     </div>
