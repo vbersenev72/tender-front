8018b10aeb89c73d59b8ad720659e93028e81f8f
Vladik <vbersenev72@gmail.com>
2024-01-20 18:35:53 +0500

    tags

--- src/pages/Tags/Tags/TagsPage.tsx
+++ src/pages/Tags/Tags/TagsPage.tsx
@@ -4,6 +4,8 @@ import Select from 'react-select'
 import './TagsPage.css'
 import { showErrorMessage, showSuccesMessage } from '../../../functions/Message';
 import axios from 'axios';
+import { LoaderTest } from '../../../styles';
+import { TailSpin } from 'react-loader-spinner';
 
 
 export interface ITagsPageProps {
@@ -13,8 +15,51 @@ export function TagsPage(props: ITagsPageProps) {
 
   const [tags, setTags] = useState<any>([])
 
+  const [loading, setLoading] = useState(false)
+
   const [createTagColor, setCreateTagColor] = useState('white')
   const [createTagName, setCreateTagName] = useState('')
+
+  const getAllTags = async () => {
+    try {
+      setLoading(true)
+      const getAllTags: any = await axios.get(`${process.env.REACT_APP_API}/api/tags/getall`, {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+
+      const newAllTags: any = []
+
+      for (let i = 0; i < getAllTags.data.message; i++) {
+
+        const tag: any = getAllTags.data.message[i];
+
+        let count = await axios.post(`${process.env.REACT_APP_API}/api/tags/getcounttenders`, {
+          idTag: tag.id
+        }, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        })
+
+        const newTag = { ...tag, count: count.data.message }
+        console.log(newTag);
+
+        newAllTags.push(newTag)
+
+      }
+
+      setTags([...newAllTags])
+      console.log(tags);
+
+      setLoading(false)
+
+
+    } catch (error: any) {
+      showErrorMessage(error.data)
+    }
+  }
 
   useEffect(() => {
 
@@ -32,38 +77,6 @@ export function TagsPage(props: ITagsPageProps) {
     { value: 'orange', label: 'Оранжевый', color: 'red' },
     { value: 'violet', label: 'Фиолетовый', color: 'red' }
   ]
-
-
-  const getAllTags = async () => {
-    try {
-
-      const getAllTags: any = await axios.get(`${process.env.REACT_APP_API}/api/tags/getall`, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem('token')}`
-        }
-      })
-
-      console.log(getAllTags.data.message);
-
-      for (let i = 0; i < getAllTags.data.message.length; i++) {
-        const tag = getAllTags.data.message[i];
-
-        const countTenders: any = await axios.post(`${process.env.REACT_APP_API}/api/tags/getcounttenders`, {
-          idTag: tag.id
-        }, {
-          headers: {
-            authorization: `Bearer ${localStorage.getItem('token')}`
-          }
-        }).then((data: any) => { setTags([...tags, { ...tag, count: data.message }]) }) ////////////
-
-      }
-
-
-    } catch (error: any) {
-      showErrorMessage(error.data)
-    }
-  }
-
 
   const createTag = async () => {
     try {
@@ -96,31 +109,41 @@ export function TagsPage(props: ITagsPageProps) {
 
 
   return (
-    <div className='TagsPage-container'>
-      <div className="TagsPage-content">
-        <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'center', fontSize: '30px', width: 'fit-content', marginBottom: '10px' }}><p>Метки</p></div>
+    <>
+      {loading ? (
+        <LoaderTest>
+          <TailSpin color="#3294F4" height={150} width={150} />
+        </LoaderTest>
+      )
+        :
+        <div className='TagsPage-container'>
+          <div className="TagsPage-content">
+            <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'center', fontSize: '30px', width: 'fit-content', marginBottom: '10px' }}><p>Метки</p></div>
 
-        <div style={{ height: '38px', display: 'flex', flexDirection: 'row' }}>
+            <div style={{ height: '38px', display: 'flex', flexDirection: 'row' }}>
 
-          <div style={{ height: '36px', width: '36px', backgroundColor: createTagColor, borderRadius: '5px', border: '1px solid gray', marginRight: '15px' }} />
+              <div style={{ height: '36px', width: '36px', backgroundColor: createTagColor, borderRadius: '5px', border: '1px solid gray', marginRight: '15px' }} />
 
-          <Select options={colorTagsOptions} onChange={(color: any) => setCreateTagColor(color.value)} />
+              <Select options={colorTagsOptions} onChange={(color: any) => setCreateTagColor(color.value)} />
 
-          <input type="text" placeholder='Название метки' style={{ height: '100%', width: '40%', marginLeft: '15px', padding: '10px', border: '1px solid gray', borderRadius: '5px' }} onChange={(e: any) => setCreateTagName(e.target.value)} value={createTagName} />
+              <input type="text" placeholder='Название метки' style={{ height: '100%', width: '40%', marginLeft: '15px', padding: '10px', border: '1px solid gray', borderRadius: '5px' }} onChange={(e: any) => setCreateTagName(e.target.value)} value={createTagName} />
 
-          <div style={{ width: '18%', backgroundColor: 'dodgerblue', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', overflow: 'hidden', cursor: 'pointer' }} onClick={() => createTag()}>Добавить</div>
+              <div style={{ width: '18%', backgroundColor: 'dodgerblue', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', overflow: 'hidden', cursor: 'pointer' }} onClick={() => createTag()}>Добавить</div>
 
+            </div>
+
+            <div>
+              {
+                tags.map((tag: any) => {
+                  <div><p>{tag.tag_name}</p></div>
+                })
+              }
+            </div>
+
+          </div>
         </div>
+      }
+    </>
 
-        {/* <div>
-          {
-            tags.map((tag: any) => {
-              <div>{tag.tag_name}</div>
-            })
-          }
-        </div> */}
-
-      </div>
-    </div>
   );
 }
