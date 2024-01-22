ad43eb77b032dedb726a0f40e083a4e1b49a0c3b
Влад Берсенев <vbersenev72@gmail.com>
2024-01-23 00:27:24 +0500

    tag

--- src/pages/Tags/TagCard/TagCard.tsx
+++ src/pages/Tags/TagCard/TagCard.tsx
@@ -7,6 +7,7 @@ import { TenderPreiewCard223, TenderPreiewCard44 } from '../../../components/Ten
 import { LoaderTest } from '../../../styles';
 import { TailSpin } from 'react-loader-spinner';
 import { RiFileExcel2Line } from 'react-icons/ri';
+import './TagCard.css'
 
 
 export interface ITagCardProps {
@@ -27,6 +28,7 @@ export function TagCard(props: ITagCardProps) {
 
   const [currentPage, setCurrentPage] = useState<any>(1)
   const [tenders, setTenders] = useState<any>([])
+  const [tag, setTag] = useState<any>({})
   const [beforeTenders, setBeforeTenders] = useState<any>([])
   const [tagTendersList, setTagTendersList] = useState<any>([])
 
@@ -52,11 +54,25 @@ export function TagCard(props: ITagCardProps) {
           }
         })
 
+        const getTag = await axios.get(`${process.env.REACT_APP_API}/api/tags/${id}`, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        })
+
+        console.log(getTag.data.message);
+        
+
+
+
+
+
         const allTenders = response.data.message
 
         setTenders([...allTenders])
         setTagTendersList([...allTenders])
         setBeforeTenders([...allTenders])
+        setTag(getTag.data.message)
 
         setLoading(false)
 
@@ -80,152 +96,143 @@ export function TagCard(props: ITagCardProps) {
 
   return (
     <div>
-<div className='TagCard-container'>
+      <div className='TagCard-container'>
 
-<div className='TagCard-content'>
-  <div className="TagCard-preview">
+        <div className='TagCard-content'>
+          <div className="TagCard-preview">
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '30px', width: 'fit-content' }}><p>Метки</p></div>
+            <div key={tag.id} style={{ display: 'flex', alignItems: 'center', padding: '25px', justifyContent: 'space-between' }}>
+
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ height: '22px', width: '22px', backgroundColor: tag.tag_color, borderRadius: '5px', marginRight: '15px' }} />
+                <p style={{ fontSize: '14px' }}>{tag.tag_name}</p>
+              </div>
+
+              <p style={{ fontSize: '12px', color: '#8F9090' }}>{tenders.length} Тендеров</p>
+            </div>
+          </div>
+
+
+          <div className="Mytenders-sort">
+            <div className='Mytenders-sort-list'>
+              <div style={{ color: 'gray', paddingLeft: '0px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingRight: '15px' }}><p>Сортировать по</p></div>
+
+              <div className="sort-property" onClick={() => {
+                setSortByDateAdded(false)
+                setSortByDateStart(true)
+                setSortByPrice(false)
+                setSortByDateFinished(false)
+                setSortByDatePublic(false)
+                //sortByDateStartTenders()
+
+              }}>
+                {
+                  !sortByDateStart
+                    ?
+                    <p>Дата начала подачи заявок</p>
+                    :
+                    <p style={{ fontWeight: 'bold' }}>Дата начала подачи заявок</p>
+                }
+              </div>
+
+              <div className="sort-property" onClick={() => {
+                setSortByDateAdded(false)
+                setSortByDateStart(false)
+                setSortByPrice(true)
+                setSortByDateFinished(false)
+                setSortByDatePublic(false)
+                //sortByPriceTenders()
+
+              }}>
+                {
+                  !sortByPrice
+                    ?
+                    <p>Цена</p>
+                    :
+                    <p style={{ fontWeight: 'bold' }}>Цена</p>
+                }
+              </div>
+
+              <div className="sort-property" onClick={() => {
+                setSortByDateAdded(false)
+                setSortByDateStart(false)
+                setSortByPrice(false)
+                setSortByDateFinished(true)
+                setSortByDatePublic(false)
+
+                //sortByDateFinishedTenders()
+
+              }}>
+                {
+                  !sortByDateFinished
+                    ?
+                    <p>Дата окончания</p>
+                    :
+                    <p style={{ fontWeight: 'bold' }}>Дата окончания</p>
+                }
+              </div>
+
+              <div className="sort-property" onClick={() => {
+                setSortByDateAdded(false)
+                setSortByDateStart(false)
+                setSortByPrice(false)
+                setSortByDateFinished(false)
+                setSortByDatePublic(true)
+
+                //sortByDatePublicTenders()
+
+              }}>
+                {
+                  !sortByDatePublic
+                    ?
+                    <p>Дата публикации</p>
+                    :
+                    <p style={{ fontWeight: 'bold' }}>Дата публикации</p>
+                }
+              </div>
+
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '15px', float: 'right' }}>
+              <RiFileExcel2Line size={30} color='#3294F4' />
+            </div>
+          </div>
+          <br />
+
+          {loading ? (
+            <LoaderTest>
+              <TailSpin color="#3294F4" height={150} width={150} />
+            </LoaderTest>
+          )
+            :
+            <div className='MyTendersList-container'>
+              <div className="MyTendersList-content">
+                {tenders
+                  .map((item: any, index: any) => (
+
+                    item ?
+                      item?.fz === 'fz223' ? (<TenderPreiewCard223 key={index} jsonData={item} myTender={true} auth={true} />) :
+                        (<TenderPreiewCard44 key={index} jsonData={item} myTender={true} auth={true} />)
+                      : null
+
+                  ))}
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+                <button onClick={() => handlePageChange((currentPage - 1))} disabled={currentPage === 1}>
+                  Назад
+                </button>
+                <span style={{ margin: '0 10px' }}>Страница {currentPage}</span>
+                <button onClick={() => {
+                  handlePageChange((currentPage + 1))
+                }}>
+                  Вперед
+                </button>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
 
     </div>
-  </div>
-
-  <div className="Mytenders-sort">
-    <div className='Mytenders-sort-list'>
-      <div style={{ color: 'gray', paddingLeft: '0px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingRight: '15px' }}><p>Сортировать по</p></div>
-
-      <div className="sort-property" onClick={() => {
-        setSortByDateAdded(true)
-        setSortByDateStart(false)
-        setSortByPrice(false)
-        setSortByDateFinished(false)
-        setSortByDatePublic(false)
-        //sortByDateAddedTenders()
-
-      }}>
-        {
-          !sortByDateAdded
-            ?
-            <p>Дата добавления в мои тендеры</p>
-            :
-            <p style={{ fontWeight: 'bold' }}>Дата добавления в мои тендеры</p>
-        }
-
-      </div>
-
-      <div className="sort-property" onClick={() => {
-        setSortByDateAdded(false)
-        setSortByDateStart(true)
-        setSortByPrice(false)
-        setSortByDateFinished(false)
-        setSortByDatePublic(false)
-        //sortByDateStartTenders()
-
-      }}>
-        {
-          !sortByDateStart
-            ?
-            <p>Дата начала подачи заявок</p>
-            :
-            <p style={{ fontWeight: 'bold' }}>Дата начала подачи заявок</p>
-        }
-      </div>
-
-      <div className="sort-property" onClick={() => {
-        setSortByDateAdded(false)
-        setSortByDateStart(false)
-        setSortByPrice(true)
-        setSortByDateFinished(false)
-        setSortByDatePublic(false)
-        //sortByPriceTenders()
-
-      }}>
-        {
-          !sortByPrice
-            ?
-            <p>Цена</p>
-            :
-            <p style={{ fontWeight: 'bold' }}>Цена</p>
-        }
-      </div>
-
-      <div className="sort-property" onClick={() => {
-        setSortByDateAdded(false)
-        setSortByDateStart(false)
-        setSortByPrice(false)
-        setSortByDateFinished(true)
-        setSortByDatePublic(false)
-
-        //sortByDateFinishedTenders()
-
-      }}>
-        {
-          !sortByDateFinished
-            ?
-            <p>Дата окончания</p>
-            :
-            <p style={{ fontWeight: 'bold' }}>Дата окончания</p>
-        }
-      </div>
-
-      <div className="sort-property" onClick={() => {
-        setSortByDateAdded(false)
-        setSortByDateStart(false)
-        setSortByPrice(false)
-        setSortByDateFinished(false)
-        setSortByDatePublic(true)
-
-        //sortByDatePublicTenders()
-
-      }}>
-        {
-          !sortByDatePublic
-            ?
-            <p>Дата публикации</p>
-            :
-            <p style={{ fontWeight: 'bold' }}>Дата публикации</p>
-        }
-      </div>
-
-    </div>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '15px', float: 'right' }}>
-      <RiFileExcel2Line size={30} color='#3294F4' />
-    </div>
-  </div>
-  <br />
-
-  {loading ? (
-    <LoaderTest>
-      <TailSpin color="#3294F4" height={150} width={150} />
-    </LoaderTest>
-  )
-    :
-    <div className='MyTendersList-container'>
-      <div className="MyTendersList-content">
-        {tenders
-          .map((item: any, index: any) => (
-
-            item ?
-              item?.fz === 'fz223' ? (<TenderPreiewCard223 key={index} jsonData={item} myTender={true} auth={true} />) :
-                (<TenderPreiewCard44 key={index} jsonData={item} myTender={true} auth={true} />)
-              : null
-
-          ))}
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-        <button onClick={() => handlePageChange((currentPage - 1))} disabled={currentPage === 1}>
-          Назад
-        </button>
-        <span style={{ margin: '0 10px' }}>Страница {currentPage}</span>
-        <button onClick={() => {
-          handlePageChange((currentPage + 1))
-        }}>
-          Вперед
-        </button>
-      </div>
-    </div>
-  }
-</div>
-
-</div>
 
   );
 }
