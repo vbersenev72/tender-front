695a73d65dd23a1cd655c5b36e8edbaf8d0cf13e
Vladik <vbersenev72@gmail.com>
2024-01-31 19:39:49 +0500

    autosearch page

--- src/components/AdvancedSearch/AdvancedSearch.tsx
+++ src/components/AdvancedSearch/AdvancedSearch.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 import './AdvancedSearch.css'
+import Select from 'react-select'
 
 export interface IAdvancedSearchProps {
 }
 
 export default function AdvancedSearch(props: IAdvancedSearchProps) {
+
+    const regionOptions =
+        [
+            { value: '', label: 'Не выбрано' },
+
+            { value: 'Регион1', label: 'Регион 1' },
+            { value: 'Регион2', label: 'Регион 2' },
+            { value: 'Регион3', label: 'Регион 3' },
+            { value: 'Регион4', label: 'Регион 4' },
+        ]
+
+    const okpd2Options = [
+
+        { value: '', label: 'Не выбрано' },
+
+        { value: 'Выбор1', label: 'Выбор 1' },
+        { value: 'Выбор1', label: 'Выбор 2' },
+        { value: 'Выбор1', label: 'Выбор 3' },
+        { value: 'Выбор1', label: 'Выбор 4' },
+    ]
+
+
+
+
     return (
         <div className='AdvancedSearch-container'>
             <div className='AdvancedSearch-content'>
@@ -32,12 +57,32 @@ export default function AdvancedSearch(props: IAdvancedSearchProps) {
 
                 </form>
                 <form className='AdvancedSearch-form'>
-                    <div className='AdvancedSearch-inputForm'></div>
-                    <div className='AdvancedSearch-inputForm'></div>
-                    <div className='AdvancedSearch-inputForm'></div>
+                    <div className='AdvancedSearch-inputForm'>
+                        <p className='AdvancedSearch-inputname'>Регион заказчика</p>
+                        <div style={{ width: '100%' }}>
+                            <Select options={regionOptions} placeholder='Не выбрано' />
+                        </div>
+                    </div>
+
+                    <div className='AdvancedSearch-inputForm'>
+                        <p className='AdvancedSearch-inputname'>Регион доставки</p>
+                        <div style={{ width: '100%' }}>
+                            <Select options={regionOptions} placeholder='Не выбрано' />
+                        </div>
+                    </div>
+
+                    <div className='AdvancedSearch-inputForm'>
+                        <p className='AdvancedSearch-inputname'>ОКПД2</p>
+                        <div style={{ width: '100%' }}>
+                            <Select options={okpd2Options} placeholder='Не выбрано' />
+                        </div>
+                    </div>
                 </form>
                 <form className='AdvancedSearch-form'>
-                    <div className='AdvancedSearch-inputForm'></div>
+                    <div className='AdvancedSearch-inputForm'>
+                        <p className='AdvancedSearch-inputname'>Номер тендера</p>
+                        <input type="text" className='AdvancedSearch-input' placeholder='Номер тендера (через пробел)' />
+                    </div>
                 </form>
             </div>
             <div className='AdvancedSearch-content'>
